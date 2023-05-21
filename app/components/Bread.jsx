c47e991470f84eb4a8d98fd2b730d7c94643b47d
mahabub-bd <palashmahabub@gmail.com>
2023-05-21 14:26:42 +0600

    add padding

--- app/components/Bread.jsx
+++ app/components/Bread.jsx
@@ -7,7 +7,7 @@ const Bread = ({mainpage,link,page}) => {
 
 
 
-    <Breadcrumb aria-label="Default breadcrumb example" className={`xl:max-w[1280px] ${styles.boxWidth} text-18 text-black`}>
+    <Breadcrumb aria-label="Default breadcrumb example" className={`xl:max-w[1280px] ${styles.boxWidth} px-3 lg:px-0 xl:px-0 text-18 text-black`}>
      <Breadcrumb.Item href={link}>{mainpage}</Breadcrumb.Item>
       <Breadcrumb.Item  >{page}</Breadcrumb.Item>
     </Breadcrumb>
